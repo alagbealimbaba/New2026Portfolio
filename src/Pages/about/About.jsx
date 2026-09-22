6f3e3f42776e23c965dc699dc7d7c179e96012c6
Alim <alagbealim81@gmail.com>
2026-09-22 15:29:04 +0100

    info

--- src/Pages/about/About.jsx
+++ src/Pages/about/About.jsx
@@ -1,4 +1,4 @@
-import Info from "../../components/info";
+import Info from "../../components/Info";
 import Stats from "../../components/Stats";
 import { FaDownload } from "react-icons/fa";
 import CV from "../../assets/NewResume2026.pdf";
